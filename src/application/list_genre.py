52e98be05d7aeeb2586ffda20197a358e6e451a8
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2024-12-08 13:48:57 -0300

    Aula 9.3 - Caso de uso ListGenre

---
+++ src/application/list_genre.py
@@ -0,0 +1,18 @@
+from enum import StrEnum
+
+from src.application.list_entity import ListEntity
+from src.application.listing import ListInput
+from src.domain.cast_member import CastMember
+from src.domain.genre import Genre
+
+
+class GenreSortableFields(StrEnum):
+    NAME = "name"
+
+
+class ListGenreInput(ListInput):
+    sort: GenreSortableFields | None = GenreSortableFields
+
+
+class ListGenre(ListEntity[Genre]):
+    pass
